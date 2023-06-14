81df24243aa6248d029e905e15a0965355d44ec5
ekyanbot <extiianmondejar@gmail.com>
2023-06-14 23:21:39 +0800

    added some t hingy

--- puddle/urls.py
+++ puddle/urls.py
@@ -3,13 +3,17 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from django.views.static import serve
+from django.conf.urls import url
+
 urlpatterns = [
     path('', include('core.urls')),
     path('items/', include('item.urls')),
     path('dashboard/', include('dashboard.urls')),
     path('inbox/', include('conversation.urls')),
     path('admin/', admin.site.urls),
-    
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
     path('oauth/', include('social_django.urls', namespace='social')),  # <-- here
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
